e9cc70d87fc6c69bc71e4bcdbe66d53b778ff8ae
iamyourkeyring <181794569+iamxoghks@users.noreply.github.com>
2025-12-02 17:01:36 +0900

    force

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -19,7 +19,28 @@ git add .
 # 특정 파일만 add 할 때(파일 경로까지 포함해야 함)
 git add test_folder/test1.txt
 
+# commit을 통해 메시지 타이틀과 메시지 내용을 commit log로 생성
+git commit -m "title" -m "content"
+
+# git commit 만 입력하고 엔터시 vi 모드. 첫 줄은 제목, 두 번째 줄부터는 내용
+git commit
+
 # commit 이력 조회
 git log
+# git log 이력 빠져나가는 키
+q
 
-git commit -m "title" -m "content"
+# log 한 줄로 보겠다
+git log --oneline
+
+# HEAD, main, origin은 뭘까?
+# origin/main, origin/HEAD
+# HEAD 하단의 로그만 보는 것이 아닌, 모든 commit 이력을 조회
+# HEAD는 현재 사용하고 있는(checkout) commit ID
+git log --all
+
+# 원격 저장소로 업로드
+git push origin 브랜치명
+# 충돌 방생시 충돌 무시하고, 로컬 기준의 코드를 원격에 덮어쓰기
+# 절대 쓰면 안 됨
+git push origin 브랜치명 --force 
