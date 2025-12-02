eab3acd250ec37dfca38db84d5444c2bf4844a17
iamyourkeyring <181794569+iamxoghks@users.noreply.github.com>
2025-12-02 15:23:03 +0900

    git day1

--- git_syntax/git_github_practice.sh
+++ git_syntax/git_github_practice.sh
@@ -20,3 +20,17 @@ git config --list
         git init
     # 2-2) 원격지 주소 추가
         git remote add origin 주소~
+
+# branch에서 master -> main 바꾸기
+git checkout -b main
+
+# 실습
+# 다른사람의 repo 가져오고 내 repo에 올리기
+# 1. 커밋 이력 그대로 가져가기
+git remote set-url origin 레포지토리 주소 #원격지 주소 변경
+
+# 2. 커밋 이력 없이 가져가기
+rm -rf .git
+git init
+git remote add origin 주소~
+git checkout -b main
